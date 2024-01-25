2c1c1dd40795ffce8c62e83a7d1dc8a85c8fa8a0
GoogleAlbum <156105425+GoogleAlbum@users.noreply.github.com>
2024-01-26 01:36:28 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-// script.js
-
 async function sendFormData() {
-    try {
-        const email = document.getElementById('email').value;
-        const address = document.getElementById('address').value;
+    const email = document.getElementById('email').value;
+    const address = document.getElementById('address').value;
 
-        if (email && address) {
-            const formData = new FormData();
-            formData.append('email', email);
-            formData.append('address', address);
+    if (email && address) {
+        const formData = new FormData();
+        formData.append('email', email);
+        formData.append('address', address);
 
+        try {
             const response = await fetch('https://formspree.io/f/meqbwbna', {
                 method: 'POST',
                 body: formData,
                 headers: {
                     'Accept': 'application/json'
-                }
+                },
             });
 
             if (response.ok) {
@@ -24,10 +22,8 @@ async function sendFormData() {
             } else {
                 console.error('Error submitting the form:', response.statusText);
             }
-        } else {
-            console.error('Email or address is missing');
+        } catch (error) {
+            console.error('Error:', error);
         }
-    } catch (error) {
-        console.error('Error:', error);
     }
 }
