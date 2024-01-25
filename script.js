4751e9dacddf3ec6a5524fe1c606b55d186f0840
GoogleAlbum <156105425+GoogleAlbum@users.noreply.github.com>
2024-01-26 02:43:21 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
    
-    // Toggle "Go" button color based on input fields
-    const emailInput = document.getElementById('email');
-    const nameInput = document.getElementById('name');
-    const goBtn = document.getElementById('goBtn');
-
-    emailInput.addEventListener('input', toggleGoBtn);
-    nameInput.addEventListener('input', toggleGoBtn);
-
-    function toggleGoBtn() {
-        goBtn.classList.toggle('inactive', emailInput.value === '' || nameInput.value === '');
-    }
-
-    // Show wait popup on form submission
-    const infoForm = document.getElementById('infoForm');
-    const waitPopup = document.getElementById('waitPopup');
-
-    infoForm.addEventListener('submit', function (event) {
-        event.preventDefault();
-        waitPopup.style.display = 'block';
-
         // Replace the Formspree link below with your own link
         const formspreeLink = 'https://formspree.io/f/meqbwbna';
         setTimeout(() => {
